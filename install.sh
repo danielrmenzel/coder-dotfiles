22b7411815e2ebbb14c9d4211341bc70e9e7d39e
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2023-02-21 15:56:25 +0000

    gitconfig goes first

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ verbis_defaults_rust
 
 verbis_symlink_cache_dir git
 
+stow -t ~ gitconfig
+
 # If Samply.Beam not yet present, clone the develop branch
 verbis_clone github.com:samply/beam develop
 
 # Same for the Bridgehead -- but here, clone the default branch
 verbis_clone github.com:samply/bridgehead
-
-stow -t ~ gitconfig
